f917a898d05483f8ece5d231bcfd7935d974a616
PhMLakeofGeneva <6897@holbertonstudents.com>
2024-02-11 16:18:26 +0000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+    """Defines a square"""
+
+
 class Square:
-    """Defines a square""" 
+"""Represent a square"""
+
     def __init__(self, size):
-        """Initialises the data"""
+        """Initialises a new sqaure"""
+
+        Args:
+            size (int): The size of the new sqaure.
+        """
         self.__size = size
