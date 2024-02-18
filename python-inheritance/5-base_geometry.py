e065719373842d488c06c52ff2bf979e1e5cb629
PhMLakeofGeneva <6897@holbertonstudents.com>
2024-02-18 14:22:26 +0000

    5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+# 5-base_geometry.py
+"""Defines an empty class BaseGeometry."""
 
 
 class BaseGeometry:
-    """empty class"""
+    """Represent base geometry."""
     pass
