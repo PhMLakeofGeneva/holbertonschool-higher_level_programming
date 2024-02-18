7e1d39c0ef790cc7d39ca3f3ec3183a36b5d3de1
PhMLakeofGeneva <6897@holbertonstudents.com>
2024-02-18 14:23:48 +0000

    6-base_geometry.py

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+# 6-base_geometry.py
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """Represent base geometry."""
 
-    @classmethod
     def area(self):
-        """method for calculated area"""
-
+        """Not implemented."""
         raise Exception("area() is not implemented")
-    
